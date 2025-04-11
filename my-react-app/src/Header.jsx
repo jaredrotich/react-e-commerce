75510c4debfe8e025b1bf548654dce6ea93d193e
jared <jaredrotich3@gmail.com>
2025-04-11 23:01:20 +0300

    structured header layout

---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,11 @@
+function Header(){
+    return(
+        <Header className="header">
+            <h1 className="header-title">
+                PRODUCT PLACE 
+            </h1>
+        </Header>
+    )
+}
+
+export default Header;
