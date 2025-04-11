fda983ffa35c75f373f79ae72e19d0ed62116597
jared <jaredrotich3@gmail.com>
2025-04-12 00:13:47 +0300

    updated button.css

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import Welcome from "./components/Welcome"
 import './App.css'
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Button from "./components/UI-elements/Button";
 
 const App = () => {
   return (
@@ -11,7 +12,18 @@ const App = () => {
       <Navbar />
       <main className="main-content">
         <Welcome name="I" title="software engineer"/>
-     
+        <div className="button-container">
+           <Button 
+             text="Click Me"
+             color="blue"
+             onClick={()=>alert('Button Clicked!')}
+           />
+           <Button 
+             text="Click Me"
+             color="green"
+             onClick={()=>alert('Button Clicked!')}
+           />
+         </div>
       </main>
       <Footer />
     </div>
