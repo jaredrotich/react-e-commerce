a6ce357116b07a11aa3a3978faf86fc78f9b2983
jared <jaredrotich3@gmail.com>
2025-04-12 00:08:03 +0300

    updated footer.jsx

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => {
+    return (
+      <footer className="footer">
+          <p>©️2025 Product Palace.™️  All rights reserved</p>
+      </footer>
+    )
+  }
+  
+  export default Footer
