2093181d8adfbc2f59ec986a07b9fa7730b7c9ef
jared <jaredrotich3@gmail.com>
2025-04-11 23:35:36 +0300

    added Welcome.jsx and updated it

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,14 @@
 import Header from "./components/Header";
+import Welcome from "./components/Welcome"
 
 const App = () => {
   return (
     <div>
       <Header />
+      <main className="main-content">
+        <Welcome name="I" title="software engineer"/>
+      
+      </main>
     </div>
   )
 }
---
+++ my-react-app/src/Welcome.jsx
@@ -0,0 +1,4 @@
+function Welcome(props){
+    console.log(props)
+    return <h2>Welcome to our React Ecommerce Website, {props.name} {props.title}!!</h2>
+}
