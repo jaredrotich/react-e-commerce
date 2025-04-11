b9758e1b0fde4e73b5ec15a83fceaa7de4349c3e
jared <jaredrotich3@gmail.com>
2025-04-11 23:59:14 +0300

    updated Navbar.jsx

---
+++ my-react-app/src/Navbar.jsx
@@ -0,0 +1,15 @@
+import "./Navbar.css"
+
+function Navbar(){
+    return (
+        <nav className="navbar">
+             <ul className="nav-list"> 
+                 <li className="nav-list-item">Home</li>
+                 <li className="nav-list-item">About</li>
+                 <li className="nav-list-item">Contact</li>
+             </ul>
+         </nav>
+    )
+}
+
+export default Navbar;
