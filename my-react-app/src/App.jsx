95b6248291b7acf05b1c1826a185824f20f93a0b
jared <jaredrotich3@gmail.com>
2025-04-11 23:53:32 +0300

    modified welcome.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,16 @@
 import Header from "./components/Header";
 import Welcome from "./components/Welcome"
+import './App.css'
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>
+    <div className="app">
       <Header />
+      <Navbar />
       <main className="main-content">
         <Welcome name="I" title="software engineer"/>
-      
+     
       </main>
     </div>
   )
--- my-react-app/src/Welcome.jsx
+++ my-react-app/src/Welcome.jsx
@@ -1,4 +1,6 @@
 function Welcome(props){
     console.log(props)
-    return <h2>Welcome to our React Ecommerce Website, {props.name} {props.title}!!</h2>
+    return <h4>Welcome to our React Ecommerce Website, {props.name} {props.title}!!</h4>
 }
+
+export default Welcome;
