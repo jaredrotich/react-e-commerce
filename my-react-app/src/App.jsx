978d65e6d61d3929641335f3259ca7b571f69c94
jared <jaredrotich3@gmail.com>
2025-04-12 16:44:59 +0300

    modified App.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,30 +4,70 @@ import './App.css'
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Button from "./components/UI-elements/Button";
+import ProductList from "./components/ProductList";
+
+const products = [
+  {
+      id: 1,
+      name: "Smartphone",
+      price: 15000,
+      description: "Latest model with 128GB data storage and a triple camera system",
+      image: "https://www.pinterest.com/pin/6614730695452071/"
+  },
+  {
+      id: 2,
+      name: "Laptop",
+      price: 150000,
+      description: "HP Victus Brand New Gaming Laptop with a 32GB dedicated graphics card",
+      image: "https://www.pinterest.com/pin/25121710417564460/"
+  },
+  {
+      id: 3,
+      name: "Smart Watch",
+      price: 5000,
+      description: "Slick feels good on the wrist. You look elegant while knowing your BMI",
+      image: "https://www.pinterest.com/pin/867013365747487991/"
+  },
+  {
+      id: 4,
+      name: "Oraimo Wireless Headphones",
+      price: 25000,
+      description: "The bass sounding is so dynamic",
+      image: "https://www.pinterest.com/pin/804244445991076218/"
+  },
+  {
+      id: 5,
+      name: "Playstation 5",
+      price: 65000,
+      description: "Enjoy the gaming experience of your life with this 2TB SSD gaming monster",
+      image: "https://www.pinterest.com/pin/122723158589412162/"
+  },
+]
 
 const App = () => {
   return (
-    <div className="app">
-      <Header />
-      <Navbar />
+    
+
+    <Navbar >
       <main className="main-content">
-        <Welcome name="I" title="software engineer"/>
+        <Welcome name="Jared" title="software engineer"/>
+        <ProductList products={products} />
         <div className="button-container">
-           <Button 
-             text="Click Me"
+           <Button
+           text="Click Me"
              color="blue"
-             onClick={()=>alert('Button Clicked!')}
-           />
-           <Button 
+             onClick={() => alert('Button Clicked!')}
+             />
+           <Button
              text="Click Me"
              color="green"
-             onClick={()=>alert('Button Clicked!')}
+             onClick={() => alert('Button Clicked!')}
            />
          </div>
       </main>
       <Footer />
-    </div>
-  )
+    </Navbar>
+  );
 }
 
 export default App
